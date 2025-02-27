0076b7ef948dd10c09535203e45efaf8102956e9
Jonathan Sosa <jososa02@wsc.edu>
2025-02-26 22:34:36 -0600

    Added JavaDoc to Student.java and Course.java. I also ended up making the displayDetailReport()

--- src/Course.java
+++ src/Course.java
@@ -14,25 +14,54 @@ import java.util.ArrayList;
  * @see <a href="https://github.com/JRBerger123">Brandon Berger's GitHub</a>
  */
 public class Course {
+    /**
+     * The Courses's name
+     */
     private String name;
+    /**
+     * The students in the course
+     */
     private ArrayList<Student> allStudents;
-    
+
+    /**Sets the student's names to "Unknown" and asigns them to a ArrayList*/
     public Course() {
         name = "unknown";
         allStudents = new ArrayList<>();
     }
-
-
+    /**
+     * Gets the course's name
+     * @return The course's name
+     */
     public String getName() {
+
         return name;
     }
 
 
+
+    /**
+     * Set's the course's name
+     * @param name the course's name
+     * @throws Exception if the course's name is blank <br>
+     */
     public void setName(String name) {
+        name = name.trim(); // remove leading and trailing whitespace
+
+        //isBlank checks for both empty or whitespace
+        if (name.isBlank())
+            throw new RuntimeException("Student name can not be blank.");
+
         this.name = name;
     }
 
-
+    /**
+     * Get a Student by using their seat number using the ArrayList method
+     * If the indexOf mtehod returns -1 then this method returns empty otherwise,
+     * it returns the Student object associated with the seat number
+     * @param seat The Student's seat
+     * @return If a Student is found, it will return the Student object otherwise a empty value
+     * @throws Exception Creating a student with a invalid seat could throw a error
+     */
     public Student getStudent(int seat) {
         for (Student s : allStudents) {
             if (s.getSeat() == seat) {
@@ -42,8 +71,15 @@ public class Course {
         return null;
     }
 
-    
-    public void addStudent(int seat, String name) {
+    /**
+     * Add a student to the course, by using the overload constructor that allows setting their name and seat too.
+     * This method will verify that the seat is not already used by another student.
+     * If it is already in use then it will throw an exception back to the calling method
+     * @param seat The student's seat
+     * @param name The course's name
+     * @throws Exception Student with seat already exists
+     */
+    public void addStudent(int seat, String name) throws Exception{
         if (getStudent(seat) != null) {
             System.out.println("Student with seat " + seat + " already exists.");
             return;
@@ -52,18 +88,47 @@ public class Course {
         allStudents.add(newStudent);
     }
 
-
+    /**
+     * Displays the Courses summary stats
+     */
     public void displaySummaryReport() {
-        System.out.println();
+
     }
 
-
+    /**
+     * Displays each Student's detailed stats for the entire course
+     */
     public void displayDetailReport() {
+        Student student;
+
+        displaySummaryReport();
+
+        System.out.println("Seat   Name            OnTime Late Excused Unexcused");
+        System.out.println("====== =============== ====== ==== ======= =========");
+
+        for (Student value : allStudents) {
+
+            student = value;
+
+            System.out.printf("%6d %-15s %5d %3d %3d %3d %5d\n",
+                    student.getName(),
+                    student.getOnTime(),
+                    student.getLate(),
+                    student.getExcused(),
+                    student.getUnexcused(),
+                    student.getSeat());
+        }
         System.out.println();
     }
 
+    /**
+     * Returns the course's name
+     * @return Course Name
+     */
     @Override
     public String toString() {
+
         return name;
     }
 }
+
--- src/Student.java
+++ src/Student.java
@@ -12,76 +12,126 @@
  * @see <a href="https://github.com/JRBerger123">Brandon Berger's GitHub</a>
  */
 public class Student {
-    /*
+    /**
      * The student's seat.
      */
     private int seat;
 
-    /*
+    /**
      * The student's name.
      */
     private String name;
 
-    /*
+    /**
      * The number of on time arrivals the students has had.
      */
     private int onTime;
 
-    /*
+    /**
      * The number of late arrivals the students has had.
      */
     private int late;
 
-    /*
+    /**
      * The number of excused absences the student has had.
      */
     private int excused;
 
-    /*
+    /**
      * The number of unexcused absences the student has had.
      */
     private int unexcused;
 
-
+    /**
+     * Sets the students fields to default values null and zeros
+     * The default constructor is Private, so it can Only be called by overload constructor
+     */
     private Student() {
         this.name = "";
         this.seat = -1;
+        this.onTime = 0;
+        this.late = 0;
+        this.excused = 0;
+        this.unexcused = 0;
     }
 
-    public Student(int seat) {
+    /**
+     * This overload constructor should only be used in the ArrayList indexOf method
+     * to detect if a seat is already taken by overriding the equals method to test
+     * if two students are equal based on their seat
+     * @param seat The Student's Seat
+     * @throws Exception if the setSeat method throws a data validation error
+     */
+    public Student(int seat) throws Exception{
         this();
+        setSeat(seat);
     }
 
-    public Student(int seat, String name) {
+    /**
+     * Allows creating of a Student and setting their name and seat number
+     * The constructor first calls the default constructor, then
+     * calls both the setName and setSeat methods
+     * @param seat The Student's seat
+     * @param name The Student's name
+     * @throws Exception if the name or seat setters throw a error
+     */
+    public Student(int seat, String name) throws Exception {
         this();
-        this.seat = seat;
-        this.name = name;
+        setSeat(seat);
+        setName(name);
     }
 
+    /**
+     * Gets the Student's Seat
+     * @return The Student's Seat
+     */
     public int getSeat() {
         return seat;
     }
 
+    /**
+     * Gets the Student's Name
+     * @return The Student's Name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the number of OnTime
+     * @return The Number of OnTime
+     */
     public int getOnTime() {
         return onTime;
     }
-
+    /**
+     * Gets the number of Late
+     * @return The Number of Late
+     */
     public int getLate() {
         return late;
     }
-
+    /**
+     * Gets the number of Excused
+     * @return The Number of Excused
+     */
     public int getExcused() {
         return excused;
     }
-
+    /**
+     * Gets the number of Unexcused
+     * @return The Number of Unexcused
+     */
     public int getUnexcused() {
         return unexcused;
     }
 
+    /**
+     * Sets the Student's Seat number if it's a positive number
+     * otherwise it throws a error
+     * @param seat the student's seat
+     * @throws Exception if the seat isn't a positive
+     */
     public void setSeat(int seat) throws Exception {
         if (seat < 1)
             throw new Exception("Seat cannot be a negative");
@@ -89,6 +139,13 @@ public class Student {
             this.seat = seat;
     } // end of setSeat Method
 
+    /**
+     * Set's the student's name. Uses the trim method to remove leading and trailing spaces
+     * and then if the name is an empty string, it will throw a error
+     * if not then it will set the student's name
+     * @param name The Student's Name
+     * @throws Exception if the student's name is blank or empty
+     */
     public void setName(String name) throws Exception {
         name = name.trim();
 
@@ -98,6 +155,16 @@ public class Student {
             this.name = name;
     } // end of setName method
 
+    /**
+     * Increments the status type, using a switch
+     * case 0 is received then increment onTime
+     * case 1 is received then increment Late
+     * case 2 is received then increment Excused
+     * case 3 is received then increment Unexcused
+     * default throw an exception displaying the invalid value that was recieved
+     * @param status The status type 0=onTime, 1=Late, 2=Excused, 3=Unexcused
+     * @throws Exception if an invalid status is received
+     */
     public void updateAttendance(int status) throws Exception {
         switch (status) {
             case 0:
@@ -116,11 +183,21 @@ public class Student {
         }
     }
 
+    /**
+     * Displays the Student's Attendance, Seat, Name
+     */
     public void displayAttendance() {
         System.out.println("Seat # " + seat + " " + name);
         System.out.println("On Time: " + onTime + " Late: " + late + " Excused: " + excused + " Unexcused: " + unexcused);
     }
 
+    /**
+     * Instead of verifying two students are identical by equal identities
+     * This method will verify two students are the same by only their seat number
+     * This allows the ArrayList method to return a player's by only checking their seat number
+     * @param object a student's object to check equality of only the seat number
+     * @return true if the two student's seat are equal otherwise false
+     */
     @Override
     public boolean equals(Object object) {
 
@@ -132,8 +209,13 @@ public class Student {
         return this.seat == other.getSeat();
     }
 
+    /**
+     * Return's the student seat number and student name
+     * @return The student's seat and name
+     */
     @Override
     public String toString() {
+
         return String.format("%s %s", seat, name);
     }
 }
